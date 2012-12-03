947e33af451727c52c2fa151828b7e4cfc6f8b18
Pablo Arias <>
2012-12-03 12:22:36 +0100

    Actualizado el framework Gantry.

--- components/com_gantry/gantry.php
+++ components/com_gantry/gantry.php
@@ -2,7 +2,7 @@
 /**
  * @package   gantry
  * @subpackage core
- * @version   4.0.5 September 18, 2012
+ * @version   4.1.4 November 22, 2012
  * @author    RocketTheme http://www.rockettheme.com
  * @copyright Copyright (C) 2007 - 2012 RocketTheme, LLC
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
@@ -15,7 +15,8 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 // Include dependancies
 jimport('joomla.application.component.controller');
+require_once(dirname(__FILE__).'/compatability.php');
 
-$controller	= JController::getInstance('Gantry');
+$controller	= GantryLegacyJController::getInstance('Gantry');
 $controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
