406e06bf224ac3cff8b52284b8b1ae8adb541106
Pablo Arias <>
2012-12-12 17:49:23 +0100

    Instalado Akeeba Backup 3.6.10.

--- logs/error.php
+++ logs/error.php
@@ -4,3 +4,4 @@
 #Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
 
 #Fields: date	time	priority	clientip	category	message
+2012-12-12	16:30:05	INFO	127.0.0.1	Joomla FAILURE: 	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
