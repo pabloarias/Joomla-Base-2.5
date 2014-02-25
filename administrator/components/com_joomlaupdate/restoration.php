5733a83985b5e5d78d40e1c6057a316a3f61e478
pablo <pablo@pablo-W110ER>
2014-02-25 01:36:04 +0100

    Modificados permisos de escritura.

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => '877PJJdHycZVVLs3pxBzb3jKoLMHsQl3',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/home/pablo/public_html/Joomla-Base/tmp/Joomla_2.5.x_to_2.5.18-Stable-Patch_Package.zip',
+	'kickstart.setup.destdir' => '/home/pablo/public_html/Joomla-Base',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
